328b1ab8b8463560db471e2a4b48f859f021a8ba
ragunathbalsubramani <ragu101010@gmail.com>
2020-01-21 05:44:04 +0530

    9 files modified

--- src/app/new-bill/new-bill.component.ts
+++ src/app/new-bill/new-bill.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewBillComponent implements OnInit {
 
+strTime:string;
+strDate:string;
+strDay:string;
+  
+
+
+
   constructor() { }
 
   ngOnInit() {
+    setInterval(() => {
+    this.setTimeUpdate();
+    },1000);
   }
+
+  setTimeUpdate() {
+   var  upDate = new Date();
+   var days =["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+   var updateMinute;
+   var hours = upDate.getHours();
+   var minutes = upDate.getMinutes();
+   var ampm = hours >= 12 ? 'PM' : 'AM';
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+     updateMinute = minutes < 10 ? '0'+minutes : minutes;
+     this.strDay = days[upDate.getDay()];
+     this.strDate = upDate.getDate()+'-'+upDate.getMonth()+1+'-'+upDate.getFullYear();
+     this.strTime = hours + ':' + updateMinute + ':'+upDate.getSeconds()+ ' ' + ampm;
+    return(this.strTime);
+    }
+
+ 
 
 }
