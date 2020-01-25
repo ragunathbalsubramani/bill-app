ce0a48b6818c5951e68b10b743542a10dd70a134
ragunathbalsubramani <ragu101010@gmail.com>
2020-01-25 07:10:05 +0530

    1 file modified

--- src/app/create-bill/create-bill.component.ts
+++ src/app/create-bill/create-bill.component.ts
@@ -7,6 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateBillComponent implements OnInit {
 
+  items = [
+    {
+        id : 1,
+        name : "sugar",
+        cat:"grocery",
+        price:44,
+        int:"kg",
+    },{
+        id : 2,
+        name : "dal",
+        cat:"grocery",
+        price:180,
+        int:"kg",
+    },{
+        id : 3,
+        name : "wheat",
+        cat:"grocery",
+        price:56,
+        int:"kg",
+    },{
+        id : 4,
+        name : "oil",
+        cat:"grocery",
+        price:165,
+        int:"kg",
+    },{
+        id : 5,
+        name : "soap",
+        cat:"grocery",
+        price:36,
+        int:"kg",
+    },{
+        id : 6,
+        name : "biscut",
+        cat:"grocery",
+        price:10,
+        int:"kg",
+    },{
+        id : 7,
+        name : "rice",
+        cat:"grocery",
+        price:55,
+        int:"kg",
+    },{
+        id : 8,
+        name : "salt",
+        cat:"grocery",
+        price:12,
+        int:"kg",
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
